bb6f8e137a81a65944734b7ae544cb2674fc1dff
KAVYA MOOLI <152363530+kavya28-hub@users.noreply.github.com>
2025-11-16 19:48:08 +0530

    Update AuthUser.js

--- models/AuthUser.js
+++ models/AuthUser.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const AuthUserSchema = new mongoose.Schema({
   name: String,
-  email: { type: String, unique: true },
-  password: String,
-  createdAt: { type: Date, default: Date.now }
+  email: String,
+  password: String
 });
 
 module.exports = mongoose.model("AuthUser", AuthUserSchema);
+
