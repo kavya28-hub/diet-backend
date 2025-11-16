d7b7432a5d48857b2a4bb20bc43d74da68d74360
KAVYA MOOLI <152363530+kavya28-hub@users.noreply.github.com>
2025-11-16 21:42:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,21 +7,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// MongoDB Connect
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB error:", err.message));
 
-// AUTH routes
-app.use("/auth", require("./routes/auth"));
-
-// Diet Recommendation API
+// only diet API
 app.use("/api", require("./routes/api"));
 
 app.get("/", (req, res) => res.send({ ok: true }));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running on", PORT));
+
 
 
 
